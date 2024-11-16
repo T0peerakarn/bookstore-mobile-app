e65d1e2c40aa2f503fe4c19b6518f01527db5709
T0peerakarn <t0p.eerakarn@gmail.com>
2024-11-17 02:40:38 +0700

    feat: add landing page, custom button

---
+++ client/component/Landing/index.tsx
@@ -0,0 +1,30 @@
+import { ImageBackground, StyleSheet, Text } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+
+import CustomBottomSheet from "./CustomBottomSheet";
+
+const LandingPage = () => {
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container} edges={[]}>
+        <GestureHandlerRootView>
+          <ImageBackground
+            source={require("../../assets/bg_landing.jpeg")}
+            style={styles.image}
+            resizeMode="cover"
+          >
+            <CustomBottomSheet />
+          </ImageBackground>
+        </GestureHandlerRootView>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+  image: { flex: 1, justifyContent: "center" },
+});
+
+export default LandingPage;
