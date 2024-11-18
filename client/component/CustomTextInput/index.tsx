0784adeaab59be41cb48c83f8d45bf67b7e92f4a
Peerakarn Thongsata <60547443+T0peerakarn@users.noreply.github.com>
2024-11-19 04:41:31 +0700

    feat: search feature (#7)

--- client/component/CustomTextInput/index.tsx
+++ client/component/CustomTextInput/index.tsx
@@ -1,3 +1,4 @@
+import React, { forwardRef } from "react";
 import { StyleSheet, TextInput, TextInputProps, View } from "react-native";
 import Icon from "react-native-vector-icons/Feather";
 
@@ -8,37 +9,43 @@ export interface ICustomTextInput extends TextInputProps {
   onPressRightIcon?: () => void;
 }
 
-const CustomTextInput = ({
-  leftIconName,
-  onPressLeftIcon,
-  rightIconName,
-  onPressRightIcon,
-  ...TextInputProps
-}: ICustomTextInput) => {
-  return (
-    <View style={styles.container}>
-      {leftIconName && (
-        <Icon
-          name={leftIconName}
-          style={{ ...styles.icon, marginRight: 6 }}
-          onPress={onPressLeftIcon}
+const CustomTextInput = forwardRef<TextInput, ICustomTextInput>(
+  (
+    {
+      leftIconName,
+      onPressLeftIcon,
+      rightIconName,
+      onPressRightIcon,
+      ...TextInputProps
+    },
+    ref
+  ) => {
+    return (
+      <View style={styles.container}>
+        {leftIconName && (
+          <Icon
+            name={leftIconName}
+            style={{ ...styles.icon, marginRight: 6 }}
+            onPress={onPressLeftIcon}
+          />
+        )}
+        <TextInput
+          ref={ref} // Pass the ref to the underlying TextInput
+          style={styles.textInput}
+          {...TextInputProps}
+          autoCapitalize="none"
         />
-      )}
-      <TextInput
-        style={styles.textInput}
-        {...TextInputProps}
-        autoCapitalize="none"
-      />
-      {rightIconName && (
-        <Icon
-          name={rightIconName}
-          style={styles.icon}
-          onPress={onPressRightIcon}
-        />
-      )}
-    </View>
-  );
-};
+        {rightIconName && (
+          <Icon
+            name={rightIconName}
+            style={styles.icon}
+            onPress={onPressRightIcon}
+          />
+        )}
+      </View>
+    );
+  }
+);
 
 const styles = StyleSheet.create({
   container: {
